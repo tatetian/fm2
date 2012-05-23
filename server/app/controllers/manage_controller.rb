f22812ec456c4286d0dd4509ffb915762738fa2e
Runchen Yan <chen_0_1987@sina.com>
2012-05-24 00:37:55 +0800

    manage static view

--- server/app/controllers/manage_controller.rb
+++ server/app/controllers/manage_controller.rb
@@ -1,4 +1,14 @@
 class ManageController < ApplicationController
+  #before_filter :signed_in_user
   def index
+      user = current_user
+      @name = user.name
+      @headurl = user.headurl
   end
+private
+  
+    def signed_in_user    
+        puts signed_in?
+        redirect_to "" unless signed_in?
+    end
 end
