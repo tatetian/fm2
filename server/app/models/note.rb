852ca92955e37467cfa5dea75dbd50480329fd2c
Runchen Yan <chen_0_1987@sina.com>
2012-06-05 22:06:05 +0800

    note and highlight feature

--- server/app/models/note.rb
+++ server/app/models/note.rb
@@ -1,11 +1,11 @@
 class Note < ActiveRecord::Base
-  attr_accessible :content, :pagenum, :paper_id, :position, :user_id
+  attr_accessible :content, :pagenum, :paper_id, :posfrom, :posto , :user_id
   
   belongs_to  :user
   belongs_to  :paper
   
   validates :user_id, presence: true
   validates :paper_id, presence: true
-  validates :content, presence: true
   
+  default_scope :order => 'notes.posfrom ASC'
 end
